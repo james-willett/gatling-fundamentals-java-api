14b63002e6908948350b6c27df231ac508e4d41e
james-willett <james.willett58@gmail.com>
2022-09-13 07:21:31 +0100

    Adding code for video 5.8

--- src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -15,24 +15,22 @@ public class VideoGameDb extends Simulation {
             .baseUrl("https://videogamedb.uk/api")
             .acceptHeader("application/json");
 
-    private ScenarioBuilder scn = scenario("Video Game Db - Section 5 code")
-
-            .exec(http("Get specific game")
-                    .get("/videogame/1")
-                    .check(status().in(200, 201, 202))
-                    .check(jmesPath("name").is("Resident Evil 4")))
-            .pause(1, 10)
-
-            .exec(http("get all video games")
+    private static ChainBuilder getAllVideoGames =
+            exec(http("Get all video games")
                     .get("/videogame")
-                    .check(status().not(404), status().not(500))
-                    .check(jmesPath("[1].id").saveAs("gameId")))
-            .pause(Duration.ofMillis(4000))
+                    .check(status().not(404), status().not(500)));
 
-            .exec(http("Get specific game with Id - #{gameId}")
-                    .get("/videogame/#{gameId}")
-                    .check(jmesPath("name").is("Gran Turismo 3"))
-                    .check(bodyString().saveAs("responseBody")));
+    private static ChainBuilder getSpecificVideoGame =
+            exec(http("Get specific video game")
+                    .get("/videogame/1")
+                    .check(status().is(200)));
+
+    private ScenarioBuilder scn = scenario("Video Game Db - Section 5 code")
+            .exec(getAllVideoGames)
+            .pause(5)
+            .exec(getSpecificVideoGame)
+            .pause(5)
+            .exec(getAllVideoGames);
 
     {
         setUp(
