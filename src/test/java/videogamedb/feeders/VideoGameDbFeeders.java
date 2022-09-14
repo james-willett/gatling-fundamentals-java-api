74204a993cfdbaa7685052765aebdc138a9aaa52
james-willett <james.willett58@gmail.com>
2022-09-14 07:26:03 +0100

    Adding code for video 6.1

---
+++ src/test/java/videogamedb/feeders/VideoGameDbFeeders.java
@@ -0,0 +1,34 @@
+package videogamedb.feeders;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class VideoGameDbFeeders extends Simulation {
+
+    private HttpProtocolBuilder httpProtocol = http
+            .baseUrl("https://videogamedb.uk/api")
+            .acceptHeader("application/json");
+
+    private static FeederBuilder.FileBased<String> csvFeeder = csv("data/gameCsvFile.csv").circular();
+
+    private static ChainBuilder getSpecificGame =
+            feed(csvFeeder)
+                    .exec(http("Get video game with name - #{gameName}")
+                    .get("/videogame/#{gameId}")
+                            .check(jmesPath("name").isEL("#{gameName}")));
+
+    private ScenarioBuilder scn = scenario("Video Game Db - Section 6 code")
+            .repeat(10).on(
+                    exec(getSpecificGame)
+                            .pause(1)
+            );
+
+    {
+        setUp(
+                scn.injectOpen(atOnceUsers(1))
+        ).protocols(httpProtocol);
+    }
+}
