9b8843a52a1e002c67f557a5f902604b5f3d8bfd
james-willett <james.willett58@gmail.com>
2022-09-23 07:44:59 +0100

    Adding code for video 7.1

---
+++ src/test/java/videogamedb/simulation/VideoGameDbSimulations.java
@@ -0,0 +1,39 @@
+package videogamedb.simulation;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class VideoGameDbSimulations extends Simulation {
+
+    private HttpProtocolBuilder httpProtocol = http
+            .baseUrl("https://videogamedb.uk/api")
+            .acceptHeader("application/json");
+
+    private static ChainBuilder getAllVideoGames =
+            exec(http("Get all video games")
+                    .get("/videogame"));
+
+    private static ChainBuilder getSpecificGame =
+            exec(http("Get specific game")
+                    .get("/videogame/2"));
+
+    private ScenarioBuilder scn = scenario("Video game db - Section 7 code")
+            .exec(getAllVideoGames)
+            .pause(5)
+            .exec(getSpecificGame)
+            .pause(5)
+            .exec(getAllVideoGames);
+
+    {
+        setUp(
+                scn.injectOpen(
+                        nothingFor(5),
+                        atOnceUsers(5),
+                        rampUsers(10).during(20)
+                ).protocols(httpProtocol)
+        );
+    }
+}
